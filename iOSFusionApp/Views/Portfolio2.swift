d2b95b34d8faddba9cc252c3da2094e83432a343
ShivamSultaniya <142833938+ShivamSultaniya@users.noreply.github.com>
2024-09-26 18:30:17 +0530

    Removed useless structs

--- iOSFusionApp/Views/Portfolio2.swift
+++ iOSFusionApp/Views/Portfolio2.swift
@@ -56,33 +56,6 @@ struct Portfolio2: View {
     }
 }
 
-// Model for assets and recommendations
-struct Asset: Identifiable {
-    let id = UUID()
-    let icon: String
-    let name: String
-    let quantity: String
-    let price: String
-    let changeAmount: String
-    let changePercentage: String
-    let isPositive: Bool
-    let color: Color
-}
-
-struct Recommendation: Identifiable {
-    let id = UUID()
-    let icon: String
-    let name: String
-    let price: String
-}
-
-let assetData = [
-    Asset(icon: "bitcoinsign.circle.fill", name: "Bitcoin", quantity: "0.5 BTC", price: "$16,405.51", changeAmount: "-$761", changePercentage: "-4.43%", isPositive: false, color: .yellow),
-    Asset(icon: "e.circle.fill", name: "Ethereum", quantity: "0.75 ETH", price: "$1,866.82", changeAmount: "+$102.2", changePercentage: "+5.79%", isPositive: true, color: .gray),
-    Asset(icon: "link.circle.fill", name: "Chainlink", quantity: "9.45 LINK", price: "$164.24", changeAmount: "+$12.3", changePercentage: "+8.09%", isPositive: true, color: .blue),
-    Asset(icon: "l.circle.fill", name: "Litecoin", quantity: "2.76 LTC", price: "$385.82", changeAmount: "+$21.79", changePercentage: "+5.98%", isPositive: true, color: .green)
-]
-
 #Preview {
     Portfolio2()
 }
